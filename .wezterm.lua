9e71891c1566c00f036adfc83e31c6772ab0a03e
rawnly <rawnly@users.noreply.github.com>
2023-10-24 13:39:05 +0200

    chore: integrate with zen-mode.nvim

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,15 +1,39 @@
 local wezterm = require("wezterm")
 
+-- Wezterm integration with folke/zen-mode.nvim
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			overrides.enable_tab_bar = false
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+			overrides.enable_tab_bar = true
+		else
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
+		end
+	end
+	window:set_config_overrides(overrides)
+end)
+
 local act = wezterm.action
 
-local wezdir = os.getenv("HOME") .. "/.wezterm"
+local wezdir = os.getenv("HOME") .. "/.config/wezterm"
 
 local config = {
 	enable_tab_bar = false,
 	default_prog = { "/opt/homebrew/bin/tmux", "new-session", "-A", "-s", "wez-main" },
 	font_size = 16,
-	color_scheme_dirs = { "$HOME/.config/wezterm/colors" },
-	color_scheme = "federico",
+	color_scheme_dirs = { wezdir .. "/colors" },
+	color_scheme = "kanagawa-dragon",
 	font = wezterm.font_with_fallback({
 		"JetBrains Mono",
 		{
