64bbae286c2819729127e0126099e799a85dccc7
rawnly <rawnly@users.noreply.github.com>
2024-02-08 21:44:35 +0100

    chore: basics

--- cargo.sh
+++ cargo.sh
@@ -1,22 +1,24 @@
+#!/usr/bin/env zsh
+
 cargo install cargo-clippy
 cargo install cargo-modules
 cargo install cargo-tauri
 cargo install cargo-add
-cargo install cargo-fmt 
+cargo install cargo-fmt
 cargo install cargo-watch
 cargo install cargo-rm
-cargo install cargo-udeps 
+cargo install cargo-udeps
 cargo install cargo-create-tauri-app
 
-cargo install bacon 
+cargo install bacon
 cargo install evcxr
 cargo install rust-gdb
 cargo install rustfmt
 cargo install wasm-pack
 cargo install bunyan
-cargo install kondo 
+cargo install kondo
 cargo install rust-gdbgui
-cargo install rustup 
+cargo install rustup
 cargo install cargo-tldr
-cargo install rslint 
+cargo install rslint
 cargo install sqlx
---
+++ install-deps.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if ! brew --version; then
+	echo "Installing Homebrew"
+
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# basics
+brew install git node go python3 oven-sh/tap/bun
+
+brew install httpie curlie
+
+# scripting
+brew install zoxide bat exa fzf fd ripgrep jq glow tokei tree broot dog dust hyperfine atuin
+
+# shell stuff
+brew install bash zsh-completions zsh-vi-mode zplug zsh-syntax-highlighting zsh-autosuggestions
+
+brew install tmux neovim helix caarlos0/tap/svu
+
+brew install --cask arc 1password spotify raycast
